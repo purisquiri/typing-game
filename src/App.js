b04b06bbf1fc8986ca9ab50a78a3eb0e639d8ea1
purisquiri <mau.dorigo@outlook.es>
2021-03-19 15:42:04 -0400

    layout, textarea and settimeout working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">"hello"</div>;
+  const [text, setText] = useState("");
+  const [count, setCount] = useState(5);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  function wordCount(str) {
+    const wordArr = str.trim().split(" ");
+    return wordArr.filter((word) => word !== "").length;
+  }
+
+  useEffect(() => {
+    if (count > 0) {
+      setTimeout(() => {
+        setCount((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+  }, [count]);
+
+  return (
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea value={text} onChange={handleChange} />
+      <h3>time remaining: {count}</h3>
+      <button onClick={() => wordCount(text)}>Start</button>
+      <h1>word count: ??? </h1>
+    </div>
+  );
 }
 
 export default App;
